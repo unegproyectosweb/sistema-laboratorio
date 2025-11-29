da5076814205255d04101219905749652e675f2c
Lorenzo Parra <alejandro36036@gmail.com>
2025-11-29 18:27:25 -0400

    feature: Property swagger
    property of login and register of swagger

--- backend/src/auth/auth.dto.ts
+++ backend/src/auth/auth.dto.ts
@@ -6,26 +6,32 @@ import {
   IsStrongPassword,
 } from "class-validator";
 import { User } from "../users/user.entity.js";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class SignInDto {
+  @ApiProperty({ type: String })
   @IsString()
   username: string;
-
+  @ApiProperty({ type: String })
   @IsString()
   password: string;
 }
 
 export class SignUpDto {
+  @ApiProperty()
   @IsString()
   username: string;
 
+  @ApiProperty()
   @IsString()
   name: string;
 
+  @ApiProperty({ type: String, required: false })
   @IsOptional()
   @IsEmail()
   email?: string | null;
 
+  @ApiProperty({ type: String })
   @IsStrongPassword(
     { minSymbols: 0, minLength: 6, minUppercase: 0, minNumbers: 0 },
     {
