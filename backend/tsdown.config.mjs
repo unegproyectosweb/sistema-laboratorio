fbe02a72cae30b2bd81bcb2c9cfcbd4ac97d82ef
David Brito <39559632+davbrito@users.noreply.github.com>
2026-01-18 19:08:05 -0400

    update deps

--- backend/tsdown.config.mjs
+++ backend/tsdown.config.mjs
@@ -1,9 +1,5 @@
 // @ts-check
-import { globSync } from "node:fs";
 import { defineConfig } from "tsdown";
-
-const migrationFiles = globSync("src/migrations/**/*.ts");
-const entityFiles = globSync("src/**/**/*.entity.ts");
 
 export default defineConfig({
   platform: "node",
@@ -11,8 +7,8 @@ export default defineConfig({
   entry: [
     "src/main.ts",
     "src/config/typeorm.ts",
-    ...migrationFiles,
-    ...entityFiles,
+    "src/migrations/**/*.ts",
+    "src/**/*.entity.ts",
   ],
   outDir: "dist",
   noExternal: [/@uneg-lab\/.*/],
