eb2f3c177192b44e7b391a02c0df1ce3c4464245
David Brito <39559632+davbrito@users.noreply.github.com>
2026-02-01 20:12:01 -0400

    feature: integrate user role management into navigation sections for enhanced access control

--- frontend/src/routes/private/layout.tsx
+++ frontend/src/routes/private/layout.tsx
@@ -1,8 +1,9 @@
 import type { NavSection } from "@/components/app-sidebar";
 import PrivateLayout from "@/components/private-layout";
-import { getSession, seemsAuthenticated } from "@/lib/auth";
+import { getSession, seemsAuthenticated, useUser } from "@/lib/auth";
 import { FileText, LayoutDashboard, Settings } from "lucide-react";
 import { Outlet, redirect } from "react-router";
+import { RoleEnum } from "@uneg-lab/api-types/auth";
 import type { Route } from "./+types/layout";
 
 const authMiddleware: Route.ClientMiddlewareFunction = async () => {
@@ -30,30 +31,41 @@ export function meta(_: Route.MetaArgs) {
   ];
 }
 
-const navSections: NavSection[] = [
-  {
-    label: "RESUMEN",
-    items: [
-      {
-        title: "Dashboard Principal",
-        href: "/dashboard",
-        icon: LayoutDashboard,
-      },
-    ],
-  },
-  {
-    label: "GESTIÓN",
-    items: [
-      { title: "Consultas y Reservas", href: "/reservas", icon: FileText },
-    ],
-  },
-  {
-    label: "CUENTA",
-    items: [{ title: "Configuración", href: "/config", icon: Settings }],
-  },
-];
-
 export default function Component() {
+  const { user } = useUser();
+
+  const navSections: NavSection[] = [
+    {
+      label: "RESUMEN",
+      items: [
+        {
+          title: "Dashboard Principal",
+          href: "/dashboard",
+          icon: LayoutDashboard,
+        },
+      ],
+    },
+    {
+      label: "GESTIÓN",
+      items: [
+        { title: "Consultas y Reservas", href: "/reservas", icon: FileText },
+      ],
+    },
+    ...(user?.role === RoleEnum.ADMIN
+      ? [
+          {
+            label: "CUENTA",
+            items: [
+              {
+                title: "Configuración",
+                href: "/config",
+                icon: Settings,
+              },
+            ],
+          },
+        ]
+      : []),
+  ];
   return (
     <PrivateLayout sections={navSections}>
       <Outlet />
